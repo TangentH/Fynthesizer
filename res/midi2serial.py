d43c261cdb0f8938a9b10f7407037d98431d6e22
Tangent-H <12112619@mail.sustech.edu.cn>
2024-05-28 20:25:08 +0800

    midi2uart python script

---
+++ res/midi2serial.py
@@ -0,0 +1,32 @@
+import subprocess
+import serial
+import time
+
+# 配置串口
+serial_port = '/dev/ttyUSB1'  # 替换为实际的串口设备
+baud_rate = 4800  # 波特率
+ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=1)
+
+# 读取MIDI信号
+def read_midi():
+    process = subprocess.Popen(['amidi', '-p', 'hw:2,0,0', '-d'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        yield line
+
+# 发送数据到串口
+def send_to_serial(data):
+    ser.write(data)
+
+try:
+    for midi_data in read_midi():
+        print(f"Read MIDI data: {midi_data}")
+        send_to_serial(midi_data)
+
+except KeyboardInterrupt:
+    print("Interrupted by user")
+
+finally:
+    ser.close()
