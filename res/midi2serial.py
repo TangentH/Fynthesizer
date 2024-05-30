a8358bb220683272309124e28de3676aaa2fcffe
Tangent-H <12112619@mail.sustech.edu.cn>
2024-05-30 23:41:26 +0800

    update midi2serial: using mido

--- res/midi2serial.py
+++ res/midi2serial.py
@@ -1,51 +1,37 @@
-import subprocess
+import mido
 import serial
-import re
 
 # 配置串口
 serial_port = '/dev/ttyUSB1'  # 替换为实际的串口设备
 baud_rate = 115200  # 波特率
 ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=1)
 
-# 读取MIDI信号
-def read_midi():
-    process = subprocess.Popen(['amidi', '-p', 'hw:2,0,0', '-d'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    while True:
-        line = process.stdout.readline()
-        if not line:
-            break
-        yield line
-
-# 发送数据到串口
 def send_to_serial(data):
     ser.write(data)
 
-def remove_whitespace_and_newlines(bitstream: str) -> str:
-    # Use regex to replace spaces and newline characters with an empty string
-    cleaned_bitstream = re.sub(r'[ \r\n]', '', bitstream)
-    return cleaned_bitstream
+def midi_to_bytes(msg):
+    return msg.bytes()
 
-def remove_whitespace_and_newlines_from_binary(binary_data: bytes) -> str:
-    # Decode binary data to a string
-    bitstream = binary_data.decode('latin1')
-    # Remove whitespace and newlines
-    cleaned_bitstream = remove_whitespace_and_newlines(bitstream)
-    return cleaned_bitstream
+# 获取可用的MIDI输入端口
+input_ports = mido.get_input_names()
+print("Available MIDI input ports:", input_ports)
 
-def hex_string_to_bytes(hex_string: str) -> bytes:
-    # Convert each pair of hex characters to their byte equivalent
-    return bytes.fromhex(hex_string)
+# 使用第一个可用的MIDI输入端口
+if input_ports:
+    input_port_name = input_ports[1]
+    print(f"Using MIDI input port: {input_port_name}")
+    
+    try:
+        with mido.open_input(input_port_name) as inport:
+            for msg in inport:
+                print(f"{msg}")
+                midi_bytes = midi_to_bytes(msg)
+                send_to_serial(midi_bytes)
 
-try:
-    for midi_data in read_midi():
-        print(f"Read MIDI data: {midi_data}")
-        midi_data_strip = remove_whitespace_and_newlines_from_binary(midi_data)
-        midi_bytes = hex_string_to_bytes(midi_data_strip)
-        # print(f"Sending bytes: {midi_bytes}")
-        send_to_serial(midi_bytes)
+    except KeyboardInterrupt:
+        print("Interrupted by user")
 
-except KeyboardInterrupt:
-    print("Interrupted by user")
-
-finally:
-    ser.close()
+    finally:
+        ser.close()
+else:
+    print("No MIDI input ports available")
